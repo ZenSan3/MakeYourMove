ed0e05451f1c2ba0fdc18bed43755dc147b1636d
ZenSan3 <cristian.zeni04@gmail.com>
2026-02-17 11:54:19 +0100

    added js doc to routes and users

--- app/routes.js
+++ app/routes.js
@@ -3,22 +3,50 @@ import Route from './models/route.js'
 const router = express.Router();
 router.use(express.json());
 
+/**
+ * return all the routes that are currently in the database
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ */
 router.get('', async (req, res)=>{
     const routes = await Route.find().exec();
     res.status(200).send(routes);
 });
 
+/**
+ * Return the routes that depart in a specific date
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {Date} date
+ */
 router.get('/all/:date', async (req, res)=>{
     const routes = await Route.find().where("dateOfDeparture").gt(req.params.date);
     res.status(200).send(routes);
 });
 
+/**
+ * Get all the routes created by a specific user
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {String} user
+ */
 router.get('/:user', async (req, res)=>{
     const routes = await Route.find({user: req.params.user}).exec();
     if(!routes) {res.status(401).json({success: false, message:'no routes created by user'})}
     res.status(200).send(routes);
 });
 
+/**
+ * Add a new station to the database
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * 
+ * the body of the request MUST HAVE: User, StationA, StationB and dateOfDeparture in JSON Format
+ */
 router.post('', async (req, res) =>{
     console.log(req.body);
     if(req.body.dateOfDeparture <= Date.now){
@@ -35,6 +63,13 @@ router.post('', async (req, res) =>{
     }
 });
 
+/**
+ * Delete a specific route using the id, because is the only univocal field
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {String} id
+ */
 router.delete('/:id', async (req, res) =>{
     console.log(req.params);
     await Route.deleteOne({_id: req.params.id})
--- app/users.js
+++ app/users.js
@@ -3,16 +3,37 @@ import User from './models/user.js'
 const router = express.Router();
 router.use(express.json());
 
+/**
+ * Return all the users present in the database
+ * 
+ * @param {*} req 
+ * @param {*} res
+ */
 router.get('', async (req, res)=>{
     const users = await User.find().exec();
     res.status(200).send(users);
 });
 
+/**
+ * Return the specific user with the username requested
+ * 
+ * @param {*} req 
+ * @param {*} res
+ * @param {String} username
+ */
 router.get('/:username', async (req, res)=>{
     const user = await User.findOne({username: req.params.username}).exec();
     res.status(200).send(user);
 });
 
+/**
+ * Add a new user to the database
+ * 
+ * @param {*} req 
+ * @param {*} res
+ * 
+ * The Body of the request MUST contain: username, email and pwd in JSON form
+ */
 router.post('', async (req, res)=>{
     console.log(req.body);
     const Nuser = await User.create({
@@ -28,6 +49,13 @@ router.post('', async (req, res)=>{
     res.status(201).send("Saved");
 });
 
+/**
+ * Delete a user with the given Email
+ * 
+ * @param {*} req 
+ * @param {*} res
+ * @param {String} email
+ */
 router.delete('/:email', async (req, res)=>{
     console.log(req.params);
     await User.deleteOne({email: req.params.email})
