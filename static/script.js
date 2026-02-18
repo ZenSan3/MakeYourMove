8035915344c827b79375931062b5e49202ed39f6
Alisia Wegher <aliport2004@gmail.com>
2026-02-18 17:54:50 +0100

    Aggiunta mappa

--- static/script.js
+++ static/script.js
@@ -5,25 +5,87 @@ function login()
     //get the form object
     var email = document.getElementById("loginEmail").value;
     var pwd = document.getElementById("loginPassword").value;
-    // console.log(email);
+    console.log(email);
+    console.log(pwd);
 
-    fetch('../api/authentication', {
+    fetch('https://makeyourmove.onrender.com/api/authentication', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify( { email: email, pwd: pwd } ),
+        body: JSON.stringify({ email: email, pwd: pwd })
     })
-    .then((res) => res.json()) // Transform the data into json
+    .then((res) => res.json())
     .then(function(data) { // Here you get the data to modify as you please
-        //console.log(data);
+        console.log(data);
+        //console.log(data.token);
         loggedUser.token = data.token;
         loggedUser.email = data.email;
         loggedUser.id = data.id;
         loggedUser.self = data.self;
         // loggedUser.id = loggedUser.self.substring(loggedUser.self.lastIndexOf('/') + 1);
         document.getElementById("loggedUser").textContent = loggedUser.email;
-        //loadLendings();
+        baseUser();
         return;
     })
     .catch( error => console.error(error) ); // If there is any error you will catch them here
 
 };
+
+function baseUser(){
+    
+    let ul = document.getElementById("Stazioni");
+    let div = document.getElementsByClassName("UserBase");
+
+    fetch('https://makeyourmove.onrender.com/api/stations')
+    .then((res) => res.json())
+    .then(function(data){
+        data.forEach(element => {
+            console.log(element);
+            
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            let a = document.createElement('a');
+            a.textContent = element.name;
+            
+            // Append all our elements
+            span.appendChild(a);
+            li.appendChild(span);
+            ul.appendChild(li);
+        });
+    })
+}
+
+
+
+
+function loadLendings() {
+
+    const ul = document.getElementById('bookLendings'); // Get the list where we will place our lendings
+
+    ul.innerHTML = '';
+
+    fetch('../api/v1/booklendings?studentId=' + loggedUser.id + '&token=' + loggedUser.token)
+    .then((resp) => resp.json()) // Transform the data into json
+    .then(function(data) { // Here you get the data to modify as you please
+        
+        console.log(data);
+        
+        return data.map( (entry) => { // Map through the results and for each run the code below
+            
+            // let bookId = book.self.substring(book.self.lastIndexOf('/') + 1);
+            
+            let li = document.createElement('li');
+            let span = document.createElement('span');
+            // span.innerHTML = `<a href="${entry.self}">${entry.book}</a>`;
+            let a = document.createElement('a');
+            a.href = entry.self
+            a.textContent = entry.book.title;
+            
+            // Append all our elements
+            span.appendChild(a);
+            li.appendChild(span);
+            ul.appendChild(li);
+        })
+    })
+    .catch( error => console.error(error) );// If there is any error you will catch them here
+    
+}
