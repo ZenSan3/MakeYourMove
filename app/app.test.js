f30829b90d26b8f4cc6cc6798bef4bf8b59ecf9b
ZenSan3 <cristian.zeni04@gmail.com>
2026-02-18 15:21:20 +0100

    added js docs to tests

--- app/app.test.js
+++ app/app.test.js
@@ -8,27 +8,31 @@ import { type } from 'os';
 const url = process.env.API
 let userToken;
 
+//Control that the app is defined
 test('app module shoud be defined', ()=>{
     expect(app).toBeDefined;
 })
 
+//control that the api is on and working
 test('GET / should return 200', ()=>{
     return request(app).get('/').expect(200);
 })
 
+//control that the api requires the authentication in the expected paths
 test('GET in users and routes should return 401 (without authentication)', async ()=>{
-    expect.assertions(2);
-    expect((await fetch(url + 'users', {method: "GET", headers: {"x-access-token": "sbagliato"}})).status).toEqual(403);
-    expect((await fetch(url + 'routes', {method: "GET", headers: {"x-access-token": "sbagliato"}})).status).toEqual(403);
-});
-
-test('GET in users and routes should return 403 (with wrong token)', async ()=>{
     expect.assertions(2);
     expect((await fetch(url + 'users')).status).toEqual(401);
     expect((await fetch(url + 'routes')).status).toEqual(401);
 });
 
+//control that the api actuallly verify the token
+test('GET in users and routes should return 403 (with wrong token)', async ()=>{
+expect.assertions(2);
+expect((await fetch(url + 'users', {method: "GET", headers: {"x-access-token": "sbagliato"}})).status).toEqual(403);
+expect((await fetch(url + 'routes', {method: "GET", headers: {"x-access-token": "sbagliato"}})).status).toEqual(403);
+});
 
+//Control that a user can authenticate
 test('Can be authenticated', async ()=>{
     expect.assertions(1);
     expect(( await fetch( url+'authentication',
@@ -59,6 +63,7 @@ test('Can be authenticated', async ()=>{
 
 })
 
+//Control that the authentication actually works
 test('GET in users and routes should return 200 (with authentication)', async ()=>{
     expect.assertions(2);
     expect((await fetch(url + 'users', {method: "GET", headers: {"x-access-token": userToken}})).status).toEqual(200);
